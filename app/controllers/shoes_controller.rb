a0a3976d096e7c05a3fe2010bee6a074e08efed2
Alfredo E. Rico Moros <alfredorico@gmail.com>
2017-08-05 23:54:54 -0400

    feature/loading_photos
    * Allow photo deletion on edit form. Note: No MDL styles applied to check_box for deletion yet.

--- app/controllers/shoes_controller.rb
+++ app/controllers/shoes_controller.rb
@@ -69,6 +69,6 @@ class ShoesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def shoe_params
-      params.require(:shoe).permit(:model, :brand, :isbn, :release_year, :sku, :photo)
+      params.require(:shoe).permit(:model, :brand, :isbn, :release_year, :sku, :photo, :remove_photo)
     end
 end
